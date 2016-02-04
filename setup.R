bd0bc74ab92e455c9406e13a42189234fe419b39
MYEONGJONG <kmj.stat@gmail.com>
2016-02-04 12:22:15 +0900

    Initializing...

---
+++ setup.R
@@ -0,0 +1,18 @@
+# 필수 패키지 설치
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("data.table")
+install.packages("lubridate")
+install.packages("Lahman")
+
+# 데이터 설치
+install.packages("babynames")
+
+
+# 패키지 로딩
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(data.table)
+
